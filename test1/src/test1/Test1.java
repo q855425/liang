4071ca72bf611b38b8d0bb8c8b9ed0c3665d0c4f
梁 <@LAPTOP-A8FFSMV7.lan>
2020-02-09 21:36:05 +0800

    er

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Test1.main()");
-                System.out.println("一giao我了giaogiao");
+		System.out.println("一giao我了giaogiao");
 	}
 }
